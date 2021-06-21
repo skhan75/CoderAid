5672cc6487a5be82d761f588e6f2a4253c834be8
skhan75 <sami.ahmadkhan12@gmail.com>
2021-06-21 02:05:40 -0700

    Optimzing max points in a line

--- Java/LeetCode/MaxPointsOnALine.java
+++ Java/LeetCode/MaxPointsOnALine.java
@@ -1,33 +1,73 @@
 
 import java.util.*;
 class MaxPointsOnALine {
-	// Points in a straight line
-        // Horizontal Line --> Same X, Y Grows
-        // Vertical Line --> Same Y, X Grows
-        // Slant Line --> Have same slope, m = (y2-y1)/(x2-x1)
-        
-        
-        // Given points we have to find what's the maximum points we can map on a slope
-        
-        // We can create a hashtable to store the key--slope<m> and value --> List<x,y>
-        // Return the key with highest number of elements in the list
-	
-	public int getMaximumPointsOnALine(int[][] points) {
-		Hashtable<Integer, LinkedList> hashtable = new Hashtable<Integer, LinkedList>();
 
-		for(int i=0; i<points.length-1; i++){
-			int y2 = points[i+1][1];
-			int y1 = points[i][1];
-
-			int x2 = points[i+1][0];
-			int x1 = points[i][0];
-
-			int m = (y2-y1)/(x2-x1);
-
-			System.out.println("Slope "+m);
+	static class Slope {
+		int numerator;
+		int denominator;
+		public Slope(int numerator, int denominator) {
+			this.numerator=numerator;
+			this.denominator=denominator;
 		}
 
-		return 0;
+		public int hashCode() {
+			return Objects.hash(numerator, denominator);
+		}
+
+		public boolean equals(Object o) {
+			if(this==o) return true;
+			if(o==null || !(o instanceof Slope)) return false;
+			Slope other = (Slope)o;
+			return other.numerator*other.denominator==numerator*denominator;
+		}
+	}
+
+	public int gcd(int a, int b) {
+		if(a==0) return b;
+		return gcd(b%a, a);
+	}
+
+	public int getMaximumPointsOnALine(int[][] points) {
+		int n = points.length, ans = 0;
+
+		if(n < 3) {
+			return n;
+		}
+
+		// Pick a point and then make every slope possible from that point and calculate the max points possible from 
+		// any of such slope
+		for(int i=0; i<n; i++) {
+
+			Map<Slope, Integer> map = new HashMap<Slope, Integer>();
+
+			int duplicate = 1, maxPoints = 1;
+
+			for(int j=i+1; j<n; j++) {
+				int dy = points[i][1] - points[j][1];
+				int dx = points[i][0] - points[j][0];
+				
+
+				if(dx==0 && dy==0) duplicate++;
+
+				else {
+					// get the greatest common factor to reduce the numerator and denominator to simplest form
+					int factor = gcd(dy, dx);
+
+					Slope slope = new Slope(dy/factor, dx/factor);
+					
+					// Add the slope to the map if not present and initialize its value as 0 if not present
+					// Get the current no of points of the current slope and then increment by 1
+					int pointsInCurrentLine = map.getOrDefault(slope, 0) + 1;
+					map.put(slope, pointsInCurrentLine);
+
+					maxPoints = Math.max(maxPoints, pointsInCurrentLine);
+				}
+			}
+
+			ans = Math.max(ans, duplicate+maxPoints);
+		}
+
+		return ans;
 
 	}
 
@@ -35,7 +75,13 @@ class MaxPointsOnALine {
 	public static void main(String[] args) {
 
 		MaxPointsOnALine maxPointsOnALine = new MaxPointsOnALine();
-		int[][] poins = {{1,1},{3,2},{5,3},{4,1},{2,3},{1,4}};
-		maxPointsOnALine.getMaximumPointsOnALine(poins);
+		int[][] points1 = {{1,1},{3,2},{5,3},{4,1},{2,3},{1,4}};
+		System.out.println("Maximum Points on a line is  "+maxPointsOnALine.getMaximumPointsOnALine(points1));
+
+		int[][] points2 = {{0,0},{1,-1},{1,1}};
+		System.out.println("Maximum Points on a line is  "+maxPointsOnALine.getMaximumPointsOnALine(points2));
+
+		int[][] points3 = {{1,1},{2,2},{3,3}};
+		System.out.println("Maximum Points on a line is  "+maxPointsOnALine.getMaximumPointsOnALine(points3));
 	}
 }
