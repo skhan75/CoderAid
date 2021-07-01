22ef7d49b3f9273f321873f7bc55169a8fd8d0ee
skhan75 <sami.ahmadkhan12@gmail.com>
2021-06-30 23:54:10 -0700

    Adding minimum coin change and rod cutting

--- Java/Dynamic Progamming/UnboundedKnapsack/MinimumCoinChange.java
+++ Java/Dynamic Progamming/UnboundedKnapsack/MinimumCoinChange.java
@@ -1,36 +1,34 @@
 import java.util.*;
+
 class MinimumCoinChange {
 
-/*
-	FOR every coin c from 0 --> coins: 
-		dp[m] = min(dp[m], dp(m - coins[c]))
-				min(without picking c'th coin, picking c'th coin)
-*/
+	/*
+	 * FOR every coin c from 0 --> coins: dp[m] = min(dp[m], dp(m - coins[c]))
+	 * min(without picking c'th coin, picking c'th coin)
+	 */
 	static int minCoins(int[] denominations, int M) {
-		int[] dp = new int[M+1];
+		int[] dp = new int[M + 1];
 		int n = denominations.length;
-		Arrays.fill(dp, Integer.MAX_VALUE-1);
+		Arrays.fill(dp, Integer.MAX_VALUE - 1);
 
 		// If M = 0, then 0 coins are required
 		dp[0] = 0;
 
-		// Iterate over every coin 
-		for(int d=0; d<n; d++) {
+		// Iterate over every coin
+		for (int d = 0; d < n; d++) {
 			// We start from denominations[d] as when m < denominations[d]
 			// We won't be able to add that denomination to required list
-			for(int m=denominations[d]; m<=M; m++) {
-				dp[m] = Math.min(dp[m],	1 + dp[m - denominations[d]]);		
+			for (int m = denominations[d]; m <= M; m++) {
+				dp[m] = Math.min(dp[m], 1 + dp[m - denominations[d]]);
 			}
 		}
 
 		return dp[M];
 	}
 
-
-
 	public static void main(String[] args) {
-		int coins[] = {9, 6, 5, 1};
-        int M = 11;
-        System.out.println ( "Minimum coins required is " + minCoins(coins, M));
+		int coins[] = { 9, 6, 5, 1 };
+		int M = 11;
+		System.out.println("Minimum coins required is " + minCoins(coins, M));
 	}
 }
