f36da5a314aeb2aec5c98ce0b45e757f0f0f5902
skhan75 <sami.ahmadkhan12@gmail.com>
2021-07-07 17:50:15 -0700

    LIS optimal in o(nlogn)

--- Java/LeetCode/LongestIncreasingSubsequence.java
+++ Java/LeetCode/LongestIncreasingSubsequence.java
@@ -63,6 +63,74 @@ class LongestIncreasingSubsequence {
 		return new LIS(maxValue, lis);
 	}
 
+	/* 	Uses Patience Sort along with Binary Search
+		Runtime Complexity --> O(NlogN)
+		Space Complexity --> O(N) 	*/
+	public LIS longestIncreasingSubsequenceOptimal(int[] A) {
+
+		// holder will store the temporary intermediate result
+		// Stores minimum of the last value of increasing subsdequence of particular length
+		int[] holder = new int[A.length];
+		// store the final result
+		int[] result = new int[A.length];
+
+		// Initialize the result with all -1s
+		for (int i=0; i<result.length;i++ ) {
+			result[i] = -1;
+		}
+
+		// Put the first index in the left most holder
+		holder[0] = 0;
+
+		int len = 0;
+
+		for(int i=1; i<A.length; i++) {
+			if(A[holder[0]] > A[i]) {
+				holder[0] = i;
+			} else if(A[holder[len]] < A[i]) { // If A[i] is greater than the last value in the holder, then put/append it in holder at that len
+				len++;
+				holder[len] = i; 
+				result[holder[len]] = holder[len-1]; // Result will store where last value at particular holder is coming from
+			} else { // Else we do a Binary Search to fdind the ceiling of A[i] and place it at that holder location
+				int ceilIndex = getCeilIndex(A, holder, len, A[i]);
+				holder[ceilIndex] = i;
+				result[holder[ceilIndex]] = holder[ceilIndex-1];
+			}
+		}
+
+		ArrayList<Integer> subsequence = new ArrayList();
+		int current = holder[len];
+		while(current!=-1) {
+			subsequence.add(A[current]);
+			current = result[current];
+		}
+
+		// reverse to get in the increasing order
+        Collections.reverse(subsequence);
+
+		return new LIS(len+1, subsequence);
+
+	}
+
+	private int getCeilIndex(int[] A, int[] holder, int end, int s) {
+		int start = 0;
+		int mid;
+		int len=end; 
+
+		while(start <= end) {
+			mid = (start + end) / 2;
+			if(mid < len && A[holder[mid]] < s && s<=A[holder[mid+1]]) { // Answer
+				return mid+1;
+			} else if(A[holder[mid]] < s) {
+				start = mid + 1;
+			} else {
+				end = mid - 1;
+			}
+		}
+
+		return -1;
+	}
+
 	
 	public static void main(String[] args) {
 		LongestIncreasingSubsequence lis = new LongestIncreasingSubsequence();
